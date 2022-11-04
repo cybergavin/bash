8c7aa0442ad643a40aaba6d0d963b4045fa4a74f
Gavin Satur <cybergavin@gmail.com>
2022-11-03 23:09:51 -0700

    Introduced more input parameters and option for checking string in HTTP response body

--- webHAT.sh
+++ webHAT.sh
@@ -8,7 +8,7 @@
 trap 'logResult && exit 200' SIGTERM SIGINT SIGKILL
 function usage
 {
-	echo -e "\nMissing arguments!\nUSAGE: bash $(basename $0) <site_URL> [number of iterations]\n\n"
+	echo -e "\nMissing arguments!\nUSAGE: bash $(basename $0) -u <gslb_url> -n <number of iterations> -b <string to match response body>\n\n"
     echo -e "If number of iterations is not specified, then the script runs indefinitely until you kill it.\n\n"
 	exit 100
 }
@@ -16,6 +16,7 @@ function checkWeb
 {
     curl_resp=`curl -k -m1 -w "\n%{http_code}\n" -s $1 2>/dev/null`
 	curl_resp_code=$(echo $curl_resp | awk '{print $NF}') 
+    curl_resp_body=$(echo $curl_resp | grep "${body_check}")
 }
 function logResult
 {
@@ -27,19 +28,44 @@ function logResult
 }
 if [ $# -eq 0 ]; then
     usage
-else
-    site_url=$1
-    num_iterations=$2
 fi
+#
+# Parse Input
+#
+while getopts ":u:n:b:" opt; do
+        case $opt in
+                u)      gslb_url=$OPTARG
+                        ;;
+                n)      num_iterations=$OPTARG
+                        ;;
+                b)      body_check=$OPTARG
+                        ;;
+                \?)     echo -e "\nInvalid option: -$OPTARG"
+                        usage
+                        ;;
+                :)      echo -e "\nOption -$OPTARG requires an argument."
+                        usage
+                        ;;
+        esac
+done
+if ((OPTIND == 1)); then
+	usage
+fi
+shift $((OPTIND-1))
+num_iterations=${num_iterations:-20}
+#
+# Main
+#
 csv_file=$(basename "${0%.*}.csv")
 echo "DATE,TIME,VALID_HIT" > $csv_file
 count=0
 valid_hits=0
+echo -n "|" # Reference for up (-) and down(_)
 if [ -n "$num_iterations" ]; then
     while [ $count -lt $num_iterations ]
         do
-            checkWeb $site_url 
-            if [ $curl_resp_code == 200 ]; then
+            checkWeb $gslb_url
+            if [ $curl_resp_code == 200 -a -n "$curl_resp_body" ]; then
                 valid_hits=$(( valid_hits + 1 ))
                 echo -n "-"
                 echo "`date '+%Y%m%d'`,`date '+%H:%M:%S'`,1" >> $csv_file
@@ -54,8 +80,8 @@ if [ -n "$num_iterations" ]; then
 else
     while [ true ]
         do
-            checkWeb $site_url 
-            if [ $curl_resp_code == 200 ]; then
+            checkWeb $gslb_url 
+            if [ $curl_resp_code == 200 -a -n "$curl_resp_body" ]; then
                 valid_hits=$(( valid_hits + 1 ))
                 echo -n "-"
                 echo "`date '+%Y%m%d'`,`date '+%H:%M:%S'`,1" >> $csv_file
